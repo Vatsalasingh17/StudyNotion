f955d0d239ecccbf95183ac331cee33e87ed1600
Vatsala Singh <singhvatsala2004@gmail.com>
2025-08-06 22:00:03 +0530

    Add files via upload

---
+++ server/models/Course.js
@@ -0,0 +1,60 @@
+const mongoose=require("mongoose");
+
+const courseSchema= new mongoose.Schema(
+    {
+        courseName:
+        {
+           type:String, 
+        },
+        courseDescription:
+        {
+            type:String,
+        },
+
+        whatYouWillLearn:
+        {
+            type:mongoose.Schema.Types.ObjectId,
+            ref:"Section",
+        },
+
+        courseContent:
+        [{
+            type:mongoose.Schema.Types.ObjectId,
+            ref:"Section",
+        }],
+
+        ratingAndReviews:
+        [
+            {
+                type:mongoose.Schema.Types.ObjectId,
+                ref:"RatingAndReview",
+            }
+        ],
+
+        price:
+        {
+            type:Number,
+        },
+        
+        thumbnail:
+        {
+            type:String,
+        },
+        tag:
+        {
+            type:mongoose.Schema.Types.ObjectId,
+            ref:"Tag",
+        },
+        studentsEnrolled:
+        [
+            {
+                type:mongoose.Schema.Types.ObjectId,
+                required:true,
+                ref:"User"
+            }
+        ]
+
+    }
+);
+
+module.exports=mongoose.model("Course",courseSchema); 
