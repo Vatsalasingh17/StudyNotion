56233eb1ecfa1554ead822b156259a01bfe6083c
Vatsala Singh <singhvatsala2004@gmail.com>
2025-10-14 11:51:57 +0530

    Update Error.jsx

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import React from 'react'  // Importing React library to use JSX
 
+// Functional component to display a 404 error message
 const Error = () => {
   return (
+    // Center the text both horizontally and vertically using Flexbox
+    // Apply large text size (3xl) and red color for emphasis
     <div className='flex justify-center items-center text-3xl text-red'>
       Error - 404 Not found
     </div>
   )
 }
 
-export default Error
+export default Error  // Exporting the component for use in routing or other parts of the app
